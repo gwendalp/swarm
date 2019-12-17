2a253c4ff419d5a0fe12b37c9ebfa22e6b87e779
gwendalp <58307678+gwendalp@users.noreply.github.com>
2019-12-17 22:24:47 +0100

    up

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 
-cd /home/gwen/Documents/CPP/EVAL/VIBES/viewer/make/;
-./VIBes-viewer &
-cd /home/gwen/Documents/CPP/EVAL;
+
 g++ main.cpp Robot.cpp vibes.cpp math.cpp -o out;
-echo "HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII";
+
 ./out
